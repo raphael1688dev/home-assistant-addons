af86b69f5e2d8bb8080013f0fbdf2d10858f9232
Martino Papero <61474670+martymarty004@users.noreply.github.com>
2024-01-07 22:14:40 +0100

    Update run.sh

--- dhcp_ipv6_server/data/run.sh
+++ dhcp_ipv6_server/data/run.sh
@@ -43,7 +43,10 @@ for network in $(bashio::config 'networks|keys'); do
     PREFIX=$(bashio::config "networks[${network}].prefix")
     INTERFACE=$(bashio::config "networks[${network}].interface")
 
-    echo "subnet6 ${SUBNET} {" >> "${CONFIG}"
+    {
+    echo "subnet6 ${SUBNET} {"
+    echo "interface ${INTERFACE};"
+    } >> "${CONFIG}"
 
     if bashio::config.has_value "networks[${network}].ranges"; then
         for i in $(bashio::config "networks[${network}].ranges|keys"); do
